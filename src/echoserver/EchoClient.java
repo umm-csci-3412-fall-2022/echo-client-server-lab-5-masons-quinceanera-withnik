0f27d736d1ae64f8fe8f3f4b45dcb4542e14b3a1
Nik Bailey <bail0301@morris.umn.edu>
2022-10-20 16:12:34 -0500

    first draft of client/server programs

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,44 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoClient {
+	public static final int portNumber =420;
+
+	public static void main(String[] args) throws IOException {
+		String server;
+		if(args.length == 0) {
+			server 	= "127.0.0.1";
+		} else {
+			server = args[0];
+		}
+
+		try {
+			// Connect to the server
+			Socket socker = new Socket(server, portNumber);
+
+			DataOutputStream out = new DataOutputStream(socker.getOutputStream());
+			out.write(System.in.read());
+
+			// Get the input stream so we can read from that socket
+			InputStream input = socker.getInputStream();
+
+			// Print all the input we recieve from the server
+			int line;
+			while ((line = input.read()) != 0) {
+				System.out.println(line);
+			}
+
+			// Close the socket when we're done reading from it
+			socker.close();
+
+			// Provide some minimal error handling.
+    		} catch (ConnectException ce) {
+      			System.out.println("We were unable to connect to " + server);
+      			System.out.println("You should make sure the server is running.");
+    		} catch (IOException ioe) {
+      			System.out.println("We caught an unexpected exception");
+      			System.err.println(ioe);
+    		}		
+	}
 }
--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,35 @@
 package echoserver;
+import java.net.*;
+import java.io.*;
 
 public class EchoServer {
+	public static final int portNumber = 6013;
+
+	public static void main(String[] args) {
+    		try {
+      			// Start listening on the specified port
+      			ServerSocket sock = new ServerSocket(portNumber);
+
+      			// Run forever, which is common for server style services
+      			while (true) {
+        			// Wait until someone connects, thereby requesting a date
+        			Socket client = sock.accept();
+        			System.out.println("Got a request!");
+
+        			// Construct a writer so we can write to the socket, thereby
+        			// sending something back to the client.
+        			PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
+
+        			// Send the current date back tothe client.
+        			writer.println(new java.util.Date().toString());
+
+        			// Close the client socket since we're done.
+        			client.close();
+      			}
+    		// *Very* minimal error handling.
+    		} catch (IOException ioe) {
+     	 		System.out.println("We caught an unexpected exception");
+      			System.err.println(ioe);
+    		}
+  	}
 }
