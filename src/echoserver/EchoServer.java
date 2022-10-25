97ff07de9b5db5fbf04429cbeef88c880be935cf
Nik Bailey <bail0301@morris.umn.edu>
2022-10-25 16:22:55 -0500

    Struggling

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,6 +1,7 @@
 package echoserver;
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class EchoServer {
 	public static final int portNumber = 6013;
@@ -15,19 +16,26 @@ public class EchoServer {
         			// Wait until someone connects, thereby requesting a date
         			Socket client = sock.accept(); // Waits until a client connects
         			System.out.println("Got a request!");
+				
+				boolean wait = true;
+				while (wait){
+					// Read from the client
+                                	Reader rdr = new InputStreamReader(client.getInputStream());
+		
+        				// Construct a writer so we can write to the socket, thereby
+        				// sending something back to the client.
+        				Writer writer = new OutputStreamWriter(client.getOutputStream());
+			
+        				// Send the current date back tothe client.	
+					writer.write(rdr.read());
+					System.out.println(rdr.read());
+					// close client
+					//if (rdr.read() == -1) {client.close(); wait = false;}
+						
+      				}
+				}	
+			
 
-				// Read from the client
-
-        			// Construct a writer so we can write to the socket, thereby
-        			// sending something back to the client.
-        			PrintWriter writer = new PrintWriter(client.getOutputStream(), true);
-
-        			// Send the current date back tothe client.
-        			writer.println(new java.util.Date().toString());
-
-        			// Close the client socket since we're done.
-        			client.close();
-      			}
     		// *Very* minimal error handling.
     		} catch (IOException ioe) {
      	 		System.out.println("We caught an unexpected exception");
